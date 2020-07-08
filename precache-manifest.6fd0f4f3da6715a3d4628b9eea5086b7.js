9d4daab113387b9300110c8b5a5c17fc26f6a6f5
rana-ghub <ssshashankrana001@gmail.com>
2020-07-09 03:37:21 +0530

    Updates

---
+++ precache-manifest.6fd0f4f3da6715a3d4628b9eea5086b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d9450ee9d216cb9433f4bf5221adad7",
+    "url": "/bulbul/index.html"
+  },
+  {
+    "revision": "a49ba097f3124343150e",
+    "url": "/bulbul/static/css/main.bfaeccbc.chunk.css"
+  },
+  {
+    "revision": "fe46af0c5fea48208355",
+    "url": "/bulbul/static/js/2.a2036cc9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bulbul/static/js/2.a2036cc9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a49ba097f3124343150e",
+    "url": "/bulbul/static/js/main.9dcd0699.chunk.js"
+  },
+  {
+    "revision": "1e5adfe81cc31a996824",
+    "url": "/bulbul/static/js/runtime-main.d11b73a1.js"
+  }
+]);
